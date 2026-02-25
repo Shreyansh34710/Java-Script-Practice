b7b5a45747202a1382982a8c970145b397b1ea5e
Shreyansh34710 <adi29shreyansh@gmail.com>
2026-02-25 13:30:22 +0530

    new change in class13

--- class13part1.js
+++ class13part1.js
@@ -1,2 +1,14 @@
 const url_3 = "https://jsonplaceholder.typicode.com/users"
 
+fetch(url_3).then((response)=>{
+    if(!response.ok){
+        throw new Error("Data not found: ",response.status)
+    }
+    return response.json()
+}).then((data)=>{
+    data.forEach((user) => {
+        console.log("user name: ",user.name)
+    });
+}).catch((error)=>{
+    console.log(error)
+})
